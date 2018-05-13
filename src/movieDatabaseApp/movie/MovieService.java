a8915bba1c2d288583a3fc4d06aa6d78da2ccd45
Sarah Cohen <cohen.sarah591@gmail.com>
2018-05-13 17:45:27 -0400

    test query change

--- src/movieDatabaseApp/movie/MovieService.java
+++ src/movieDatabaseApp/movie/MovieService.java
@@ -4,12 +4,13 @@ import movieDatabaseApp.movie.MovieFeedModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MovieService {
 	@GET("?source=http://hydramovies.com/api-v2/current-Movie-Data.csv")
 	Call<MovieFeedModel> getAllMovies();
 	
-	@GET("?source=http://hydramovies.com/api-v2/current-Movie-Data.csv&title={range}")
-	Call<MovieFeedModel> getMovieByTitle(@Path("range") String range);
+	@GET("?source=http://hydramovies.com/api-v2/current-Movie-Data.csv")
+	Call<MovieFeedModel> getMovieByTitle(@Query("range") String range);
 	
 }
